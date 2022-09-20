84311706475290109c5e713e348452f41400a7a3
huseyin.altinok <huseyin.altinok@HUSEYINK>
2022-09-20 15:46:40 +0300

    Add project files.

---
+++ HealthCheckDemo.Service1/HealthChecks/ResponseTimeHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheckDemo.Service1.HealthChecks
+{
+    public class ResponseTimeHealthCheck : IHealthCheck
+    {
+        public Random random = new Random();
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int responseTimeInMs = random.Next(1, 300);
+
+            if (responseTimeInMs < 100)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy($"Kaymak gibi çalışıyor ({responseTimeInMs}). "));
+            }
+            else if(responseTimeInMs < 200)
+            {
+                return Task.FromResult(HealthCheckResult.Degraded($"Azcık yavaş çalışıyor ({responseTimeInMs}). "));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy($"Aloooooo Çalışmıyoooooooo ({responseTimeInMs}). "));
+            }
+        }
+
+
+    }
+}
---
+++ HealthCheckDemo.Service1/Startup.cs
@@ -0,0 +1,81 @@
+﻿
+using HealthCheckDemo.Service1.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "HealthCheckDemo.Service1", Version = "v1" });
+        });
+
+
+        services.AddHealthChecks()
+            .AddCheck<ResponseTimeHealthCheck>("Network Speed Test", null, new[] { "service" });
+            //.AddSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+            //"SELECT 1;",
+            //"Pazartane_Test_Supports",
+            ////HealthStatus.Healthy,
+            //timeout: TimeSpan.FromSeconds(5),
+            //tags: new[] { "db", "sql", "sqlserver" })
+
+            //.AddRedis("ada");
+
+
+        services.AddSingleton<ResponseTimeHealthCheck>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HealthCheckDemo.Service1 v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+
+            //endpoints.MapHealthChecks("/quickhc", new HealthCheckOptions()
+            //{
+            //    Predicate = _ => false
+            //});
+
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            endpoints.MapControllers();
+        });
+    }
+}
